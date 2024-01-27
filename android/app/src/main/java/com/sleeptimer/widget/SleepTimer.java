75acf315cbc9809a087fdb4da1973b71fa83d023
cenkozk <emirozkan61@hotmail.com>
2024-01-27 20:20:02 +0300

    Final for a while.

---
+++ android/app/src/main/java/com/sleeptimer/widget/SleepTimer.java
@@ -0,0 +1,14 @@
+package com.sleeptimer;
+import com.reactnativeandroidwidget.RNWidgetProvider;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class SleepTimer extends RNWidgetProvider {
+
+}
